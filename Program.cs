543a0c454f24fa3bf6099820ebfa8c472ce3a0a1
mycookie5 <fanual.abrahm@gmail.com>
2024-09-27 11:07:42 +0200

    firts commit :)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+public class Product
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public string Category { get; set; }
+
+    public Product(string name, int price, string category)
+    {
+        Name = name;
+        Price = price;
+        Category = category;
+    }
+
+    public void DisplayPrice()
+    {
+        Console.WriteLine($"Price: {Price}");
+    }
+}
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Product groupA = new Product("Apple", 10000, "Fruit");
+        groupA.DisplayPrice();
+    }
+}
